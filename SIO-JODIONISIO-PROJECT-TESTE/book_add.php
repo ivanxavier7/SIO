f3b51ea60ad0ac379af28a434b64d66fbbde41e5
Gam3las <fgamelas11@ua.pt>
2021-10-25 19:44:54 +0100

    Switched placeholders instead of "smallS advices on book_add.php

--- SIO-JODIONISIO-PROJECT-TESTE/book_add.php
+++ SIO-JODIONISIO-PROJECT-TESTE/book_add.php
@@ -7,18 +7,18 @@
         <form action="book_add.php" method="POST">
             <div class="form-group">
               <label for="name">Name</label>
-              <input type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId">
-              <small id="helpId" class="text-muted">Please insert the book name</small>
+              <input type="text" name="name" id="" class="form-control" placeholder="Please insert the book name" aria-describedby="helpId">
+              <!--<small id="helpId" class="text-muted">Please insert the book name</small>-->
             </div>
             <div class="form-group">
               <label for="author">Author</label>
-              <input type="text" name="author" id="" class="form-control" placeholder="" aria-describedby="helpId">
-              <small id="helpId" class="text-muted">Please inser the book author</small>
+              <input type="text" name="author" id="" class="form-control" placeholder="Please inser the book author" aria-describedby="helpId">
+              <!--<small id="helpId" class="text-muted">Please inser the book author</small>-->
             </div>
             <div class="form-group">
               <label for="price">Price</label>
-              <input type="number" step="any" name="price" id="" class="form-control" placeholder="" aria-describedby="helpId">
-              <small id="helpId" class="text-muted">Please inser the book price</small>
+              <input type="number" step="any" name="price" id="" class="form-control" placeholder="Please inser the book price" aria-describedby="helpId">
+              <!--<small id="helpId" class="text-muted">Please inser the book price</small>-->
             </div>
             
             <button type="submit" name="submit" class="btn btn-primary">Submit</button>
